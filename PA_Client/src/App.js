85420e96889632d23b0caf52f7783ac48357d7df
Yan Wong <kelvinwong26546@gmail.com>
2022-10-08 22:11:23 +0800

    structure app

---
+++ PA_Client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App ">
+      <p className='text-2xl p-10'>
+        Hello
+      </p>
+    </div>
+  );
+}
+
+export default App;
