d54ba43ac4e26c6397c829609bc63be1355797e0
Yan Wong <kelvinwong26546@gmail.com>
2022-10-09 17:26:39 +0800

    styling form

--- PA_Client/src/pages/Landing.js
+++ PA_Client/src/pages/Landing.js
@@ -24,45 +24,43 @@ function Landing() {
     }
 
     return (
-        <dix className='w-screen flex justify-center '>
+        <div className='w-screen flex justify-center'>
             <div className='w-fit h-screen flex flex-col justify-center '>
                 <h1 className='w-fit text-2xl bg-cyan-300 p-5'>Welcome To Personal Assistant</h1>
-                <div className='w-full p-3 bg-teal-300'>
-                    <form 
-                    className='flex flex-col p-2 space-y-4 '
-                    onSubmit={handleLoginSubmit}>
-                        <label>Username</label>
-                        <input 
-                        type="text"
-                        maxLength={50}
-                        placeholder='Username'
-                        required={true}
-                        onChange={(e)=>setLoginUser(e.target.value)}
-                        />
-                        <label>Password</label>
-                        <input 
-                        type="text"
-                        maxLength={50}
-                        placeholder='Password'
-                        required={true}
-                        onChange={(e)=>setLoginPass(e.target.value)}
-                        />
-                        <button 
-                        className='bg-teal-500'
-                        type='submit'>Login</button>
-                        </form>
-                        <div className='flex justify-between p-2'>
-                            <h3
-                            className=''
-                            >Not have an account yet?</h3>
-                            <NavLink
-                            to='/signup'
-                            className='bg-teal-500 p-1'
-                            >Sign up</NavLink>
-                        </div>
-                </div>
+                <form 
+                className='w-full bg-teal-300 flex flex-col p-5 space-y-4 '
+                onSubmit={handleLoginSubmit}>
+                    <label>Username</label>
+                    <input 
+                    type="text"
+                    maxLength={50}
+                    placeholder='Username'
+                    required={true}
+                    onChange={(e)=>setLoginUser(e.target.value)}
+                    />
+                    <label>Password</label>
+                    <input 
+                    type="text"
+                    maxLength={50}
+                    placeholder='Password'
+                    required={true}
+                    onChange={(e)=>setLoginPass(e.target.value)}
+                    />
+                    <button 
+                    className='bg-teal-500'
+                    type='submit'>Login</button>
+                    </form>
+                    <div className='flex justify-between p-5 pt-0 w-full bg-teal-300'>
+                        <h3
+                        className=''
+                        >Not have an account yet?</h3>
+                        <NavLink
+                        to='/signup'
+                        className='bg-teal-500 p-1'
+                        >Sign up</NavLink>
+                    </div>
             </div>
-        </dix>
+        </div>
     )
 }
 
--- PA_Client/src/pages/SignUp.js
+++ PA_Client/src/pages/SignUp.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { NavLink } from 'react-router-dom';
 import { registerUser } from '../utlis/User';
 
 function SignUp() {
@@ -18,43 +19,51 @@ function SignUp() {
     }
 
   return (
-    <div className='w-vh h-vh' id='register'>
-        <h3>
-            create account
-        </h3>
-        <form
-        onSubmit={handleRegisterSubmit}
-        >
-            <label>Username</label>
-            <input 
-            type="text"
-            maxLength={50}
-            placeholder='Username'
-            required={true}
-            onChange={(e)=>setCreateUser(e.target.value)}
-            />
-            <label>Password</label>
-            <input 
-            type="text"
-            maxLength={50}
-            placeholder='Password'
-            required={true}
-            onChange={(e)=>setCreatePass(e.target.value)}
-            />
-            <label>Email</label>
-            <input 
-            type="email"
-            maxLength={50}
-            placeholder='Email'
-            onChange={(e)=>setCreateEmail(e.target.value)}
-            />
-            <button 
-            
-            type='submit'>
-                Register
-            </button>
-
-            </form>
+    <div className='w-screen h-screen flex justify-center'>
+        <div className='w-[500px] h-screen flex flex-col justify-center'>
+            <h3 className='w-full text-2xl bg-cyan-300 p-5'>
+                Create account
+            </h3>
+            <form
+            className='w-full bg-teal-300 flex flex-col p-5 space-y-4'
+            onSubmit={handleRegisterSubmit}
+            >
+                <label>Username</label>
+                <input 
+                type="text"
+                maxLength={50}
+                placeholder='Username'
+                required={true}
+                onChange={(e)=>setCreateUser(e.target.value)}
+                />
+                <label>Password</label>
+                <input 
+                type="text"
+                maxLength={50}
+                placeholder='Password'
+                required={true}
+                onChange={(e)=>setCreatePass(e.target.value)}
+                />
+                <label>Email</label>
+                <input 
+                type="email"
+                maxLength={50}
+                placeholder='Email'
+                onChange={(e)=>setCreateEmail(e.target.value)}
+                />
+                <button 
+                className='bg-teal-500'
+                type='submit'>
+                    Register
+                </button>
+                </form>
+                <div className='p-5 pt-0 w-full bg-teal-300'>
+                        <NavLink
+                        to='/'
+                        className='bg-teal-500 p-1'
+                        >Back To Login</NavLink>
+                    </div>
+            </div>
         </div>
   )
 }
